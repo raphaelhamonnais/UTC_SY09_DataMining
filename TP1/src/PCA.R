f3825b6dd27f6e9ea01e2b7196f5dc6854c08391
raphaelhamonnais <raphael.hamonnais@gmail.com>
2017-03-21 18:31:43 +0100

    suite ACP

--- TP1/src/PCA.R
+++ TP1/src/PCA.R
@@ -1,18 +1,26 @@
-notes <- read.csv("E:/study/UTC/GI05/SY09/TD/TD1/donnees/sy02-p2016.csv", na.strings="", header=T)
+notes <- read.csv("data/sy02-p2016.csv", na.strings="", header=T)
 notes$nom <- factor(notes$nom, levels=notes$nom)
 notes$niveau <- factor(notes$niveau, ordered=T)
 notes$resultat <- factor(notes$resultat, levels=c("F","Fx","E","D","C","B","A"),
                          ordered=T)
+notes$correcteur.median <- factor(notes$correcteur.median, levels=c("Cor1","Cor2","Cor3","Cor4","Cor5","Cor6","Cor7","Cor8"))
+notes$correcteur.final <- factor(notes$correcteur.final, levels=c("Cor1","Cor2","Cor3","Cor4","Cor5","Cor6","Cor7","Cor8"))
 boxplot(notes)
 summary(notes)
+
+
+
+###########################################
+# 2.1 Exercice théorique
 
 #calculer les moyennes des notes médian corrigées par chaque correcteur 
 #aggregate: Splits the data into subsets, computes summary statistics for each, and returns the result in a convenient form.
 moy.median <- aggregate(note.median~correcteur.median, data=notes, FUN=mean)
 #renommer la dataframe “moy.median”
+moy.median
 names(moy.median) <- c("correcteur","moy.median")
 moy.median
-#calculer les variances des notes médian corrigées par chaque correcteur 
+#calculer les écart-types des notes médian corrigées par chaque correcteur 
 std.median <- aggregate(note.median~correcteur.median, data=notes, FUN=sd)
 names(std.median) <- c("correcteur","std.median")
 std.median
@@ -23,19 +31,46 @@ median
 moy.final <- aggregate(note.final~correcteur.final, data=notes, FUN=mean)
 names(moy.final) <- c("correcteur","moy.final")
 moy.final
-#calculer les variances des notes finales corrigées par chaque correcteur 
+#calculer les écarts types des notes finales corrigées par chaque correcteur 
 std.final <- aggregate(note.final~correcteur.final, data=notes, FUN=sd)
 names(std.final) <- c("correcteur","std.final")
 std.final
 final <- merge(moy.final, std.final)
 final
-# correcteurs <- merge(median, final, all=T)
-# correcteurs
-# 
-# corr.acp <- correcteurs[-c(2,8),]
-# corr.acp
-#supprimer deux lignes avec des resultats NAs
-correcteurs <- merge(median, final)
+correcteurs <- merge(median, final, all=T)
+
+# supprimer deux lignes avec des resultats NAs
 correcteurs
+corr.acp <- correcteurs[-c(2,3),] # on peut aussi faire merge(median, final) sans le "all=T"
+corr.acp
 
 
+# 1. Calculer les axes factoriels de l’ACP du nuage de points défini
+# par les quatre variables quantitatives.
+# Quels sont les pourcentages d’inertie expliquée par chacun de ces axes ?
+corr.acp <- corr.acp[,-c(1)] # ne prendre que les variables quantitatives
+corr.acp
+corr.acp.centered = scale(corr.acp, center = TRUE, scale = FALSE) # centrer les données en colonne
+corr.acp.centered
+corr.acp.covar = cov.wt(corr.acp.centered, method = "ML") # calcul de matrice de covariance avec centrage automatique
+corr.acp.covar
+corr.acp.covar.diago = eigen(corr.acp.covar$cov) # diagonaliser la covariance (pas les moyennes)
+val_propres = corr.acp.covar.diago$values # valeurs propres = quantités d'inertie expliquée
+vec_propres = corr.acp.covar.diago$vectors # vecteurs propres = vecteurs qui portent/expliquent les axes factoriels
+# normalement matrice de covariance = corr.acp.covar.diago$vectors * corr.acp.covar.diago$values * t(corr.acp.covar.diago$vectors)
+vec_propres %*% diag(val_propres) %*% t(vec_propres)
+corr.acp.covar$cov
+# on remarque que les deux sont egales, même si le comparateur "==" ne marche pas car pas problème de précision
+
+# Calculer les axes factoriels de l’ACP
+# les axes factoriels sont donnés par les vecteurs propres situés dans vec_propres : chaque colonne est un des vecteur propre
+vec_propres 
+# les quantités d'inertie expliquées sont dans la variable val_propres
+val_propres
+# les pourcentages d'inerties sont simplement la quantité / la somme de la quantité totale d'inertie
+pourcentage_inertie = val_propres / sum(val_propres) * 100
+pourcentage_inertie
+
+# 2. Calculer les composantes principales ; en déduire la représentation des quatre individus dans le premier plan factoriel.
+ACP = corr.acp.centered %*% vec_propres
+ACP # coordonnées des points sur le nouveau repère des composantes principales = combinaisons linéaires des anciens axes
