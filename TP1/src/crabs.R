6d58d9d49d1494315c3691265acd14e3e715319e
raphaelhamonnais <raphael.hamonnais@gmail.com>
2017-03-22 14:11:57 +0100

    Modif crab.R : ajout labels pour les boxplot

--- TP1/src/crabs.R
+++ TP1/src/crabs.R
@@ -72,15 +72,15 @@ Crabs_Male_and_Blue
 Crabs_Male_and_Orange <- Crabs_Male[! Male_Indexes == Blue_Indexes,] # TODO [Male_Indexes == Orange_Indexes] ne marche pas, pourquoi ???
 Crabs_Male_and_Orange
 #comparer le paramètre FL de crabe male en fonction de l'espèce (bleue ou orange)
-boxplot(Crabs_Male_and_Blue$FL,Crabs_Male_and_Orange$FL,col = c("blue","orange"), main = "Males - Comparaison de la variable FL selon l'espèce")
+boxplot(Crabs_Male_and_Blue$FL, Crabs_Male_and_Orange$FL, col = c("blue","orange"), names = c("Esp. Bleue","Esp. Orange"), main = "Males - Comparaison de la variable FL selon l'espèce")
 #comparer le paramètre RW de crabe male en fonction de l'espèce (bleue ou orange)
-boxplot(Crabs_Male_and_Blue$RW,Crabs_Male_and_Orange$RW,col = c("blue","orange"), main = "Males - Comparaison de la variable RW selon l'espèce")
+boxplot(Crabs_Male_and_Blue$RW, Crabs_Male_and_Orange$RW, col = c("blue","orange"), names = c("Esp. Bleue","Esp. Orange"), main = "Males - Comparaison de la variable RW selon l'espèce")
 #comparer le paramètre CL de crabe male en fonction de l'espèce (bleue ou orange)
-boxplot(Crabs_Male_and_Blue$CL,Crabs_Male_and_Orange$CL,col = c("blue","orange"), main = "Males - Comparaison de la variable CL selon l'espèce")
+boxplot(Crabs_Male_and_Blue$CL, Crabs_Male_and_Orange$CL, col = c("blue","orange"), names = c("Esp. Bleue","Esp. Orange"), main = "Males - Comparaison de la variable CL selon l'espèce")
 #comparer le paramètre CW de crabe male en fonction de l'espèce (bleue ou orange)
-boxplot(Crabs_Male_and_Blue$CW,Crabs_Male_and_Orange$CW,col = c("blue","orange"), main = "Males - Comparaison de la variable CW selon l'espèce")
+boxplot(Crabs_Male_and_Blue$CW, Crabs_Male_and_Orange$CW, col = c("blue","orange"), names = c("Esp. Bleue","Esp. Orange"), main = "Males - Comparaison de la variable CW selon l'espèce")
 #comparer le paramètre BD de crabe male en fonction de l'espèce (bleue ou orange)
-boxplot(Crabs_Male_and_Blue$BD,Crabs_Male_and_Orange$BD,col = c("blue","orange"), main = "Males - Comparaison de la variable BD selon l'espèce")
+boxplot(Crabs_Male_and_Blue$BD, Crabs_Male_and_Orange$BD, col = c("blue","orange"), names = c("Esp. Bleue","Esp. Orange"), main = "Males - Comparaison de la variable BD selon l'espèce")
 
 #crabes femelles (index 51-100 et 151-200)
 Crabs_Female <- crabsquant[Female_Indexes,]
@@ -92,39 +92,39 @@ Crabs_Female_and_Blue
 Crabs_Female_and_Orange <- Crabs_Female[Female_Indexes == Orange_Indexes,]
 Crabs_Female_and_Orange
 #comparer le paramètre FL de crabe femelle  en fonction de l'espèce (bleue ou orange)
-boxplot(Crabs_Female_and_Blue$FL,Crabs_Female_and_Orange$FL,col = c("blue","orange"), main = "Femelles - Comparaison de la variable FL selon l'espèce")
+boxplot(Crabs_Female_and_Blue$FL, Crabs_Female_and_Orange$FL, col = c("blue","orange"), names = c("Esp. Bleue","Esp. Orange"), main = "Femelles - Comparaison de la variable FL selon l'espèce")
 #comparer le paramètre RW de crabe femelle  en fonction de l'espèce (bleue ou orange)
-boxplot(Crabs_Female_and_Blue$RW,Crabs_Female_and_Orange$RW,col = c("blue","orange"), main = "Femelles - Comparaison de la variable RW selon l'espèce")
+boxplot(Crabs_Female_and_Blue$RW, Crabs_Female_and_Orange$RW, col = c("blue","orange"), names = c("Esp. Bleue","Esp. Orange"), main = "Femelles - Comparaison de la variable RW selon l'espèce")
 #comparer le paramètre CL de crabe femelle  en fonction de l'espèce (bleue ou orange)
-boxplot(Crabs_Female_and_Blue$CL,Crabs_Female_and_Orange$CL,col = c("blue","orange"), main = "Femelles - Comparaison de la variable CL selon l'espèce")
+boxplot(Crabs_Female_and_Blue$CL, Crabs_Female_and_Orange$CL, col = c("blue","orange"), names = c("Esp. Bleue","Esp. Orange"), main = "Femelles - Comparaison de la variable CL selon l'espèce")
 #comparer le paramètre CW de crabe femelle  en fonction de l'espèce (bleue ou orange)
-boxplot(Crabs_Female_and_Blue$CW,Crabs_Female_and_Orange$CW,col = c("blue","orange"), main = "Femelles - Comparaison de la variable CW selon l'espèce")
+boxplot(Crabs_Female_and_Blue$CW, Crabs_Female_and_Orange$CW, col = c("blue","orange"), names = c("Esp. Bleue","Esp. Orange"), main = "Femelles - Comparaison de la variable CW selon l'espèce")
 #comparer le paramètre BD de crabe femelle  en fonction de l'espèce (bleue ou orange)
-boxplot(Crabs_Female_and_Blue$BD,Crabs_Female_and_Orange$BD,col = c("blue","orange"), main = "Femelles - Comparaison de la variable BD selon l'espèce")
+boxplot(Crabs_Female_and_Blue$BD, Crabs_Female_and_Orange$BD, col = c("blue","orange"), names = c("Esp. Bleue","Esp. Orange"), main = "Femelles - Comparaison de la variable BD selon l'espèce")
 
 
 #etudier selon le sexe
 #comparer le paramètre FL des crabes d'espèce bleue en fonction du sexe (male/femelle)
-boxplot(Crabs_Male_and_Blue$FL,Crabs_Female_and_Blue$FL,col = c("blue","blue"), main = "Espèce Bleue - Comparaison de la variable FL selon le sexe")
+boxplot(Crabs_Male_and_Blue$FL, Crabs_Female_and_Blue$FL, col = c("grey","white"), names = c("Mâle","Femelle"), main = "Espèce Bleue - Comparaison de la variable FL selon le sexe")
 #comparer le paramètre RW des crabes d'espèce bleue en fonction du sexe (male/femelle)
-boxplot(Crabs_Male_and_Blue$RW,Crabs_Female_and_Blue$RW,col = c("blue","blue"), main = "Espèce Bleue - Comparaison de la variable RW selon le sexe")
+boxplot(Crabs_Male_and_Blue$RW, Crabs_Female_and_Blue$RW, col = c("grey","white"), names = c("Mâle","Femelle"), main = "Espèce Bleue - Comparaison de la variable RW selon le sexe")
 #comparer le paramètre CL des crabes d'espèce bleue en fonction du sexe (male/femelle)
-boxplot(Crabs_Male_and_Blue$CL,Crabs_Female_and_Blue$CL,col = c("blue","blue"), main = "Espèce Bleue - Comparaison de la variable CL selon le sexe")
+boxplot(Crabs_Male_and_Blue$CL, Crabs_Female_and_Blue$CL, col = c("grey","white"), names = c("Mâle","Femelle"), main = "Espèce Bleue - Comparaison de la variable CL selon le sexe")
 #comparer le paramètre CW des crabes d'espèce bleue en fonction du sexe (male/femelle)
-boxplot(Crabs_Male_and_Blue$CW,Crabs_Female_and_Blue$CW,col = c("blue","blue"), main = "Espèce Bleue - Comparaison de la variable CW selon le sexe")
+boxplot(Crabs_Male_and_Blue$CW, Crabs_Female_and_Blue$CW, col = c("grey","white"), names = c("Mâle","Femelle"), main = "Espèce Bleue - Comparaison de la variable CW selon le sexe")
 #comparer le paramètre BD des crabes d'espèce bleue en fonction du sexe (male/femelle)
-boxplot(Crabs_Male_and_Blue$BD,Crabs_Female_and_Blue$BD,col = c("blue","blue"), main = "Espèce Bleue - Comparaison de la variable BD selon le sexe")
+boxplot(Crabs_Male_and_Blue$BD, Crabs_Female_and_Blue$BD, col = c("grey","white"), names = c("Mâle","Femelle"), main = "Espèce Bleue - Comparaison de la variable BD selon le sexe")
 
 #comparer le paramètre FL des crabes d'espèce orange en fonction du sexe (male/femelle)
-boxplot(Crabs_Male_and_Orange$FL,Crabs_Female_and_Orange$FL,col = c("orange","orange"), main = "Espèce Orange - Comparaison de la variable FL selon le sexe")
+boxplot(Crabs_Male_and_Orange$FL, Crabs_Female_and_Orange$FL, col = c("grey","white"), names = c("Mâle","Femelle"), main = "Espèce Orange - Comparaison de la variable FL selon le sexe")
 #comparer le paramètre FL des crabes d'espèce orange en fonction du sexe (male/femelle)
-boxplot(Crabs_Male_and_Orange$RW,Crabs_Female_and_Orange$RW,col = c("orange","orange"), main = "Espèce Orange - Comparaison de la variable RW selon le sexe")
+boxplot(Crabs_Male_and_Orange$RW, Crabs_Female_and_Orange$RW, col = c("grey","white"), names = c("Mâle","Femelle"), main = "Espèce Orange - Comparaison de la variable RW selon le sexe")
 #comparer le paramètre FL des crabes d'espèce orange en fonction du sexe (male/femelle)
-boxplot(Crabs_Male_and_Orange$CL,Crabs_Female_and_Orange$CL,col = c("orange","orange"), main = "Espèce Orange - Comparaison de la variable CL selon le sexe")
+boxplot(Crabs_Male_and_Orange$CL, Crabs_Female_and_Orange$CL, col = c("grey","white"), names = c("Mâle","Femelle"), main = "Espèce Orange - Comparaison de la variable CL selon le sexe")
 #comparer le paramètre FL des crabes d'espèce orange en fonction du sexe (male/femelle)
-boxplot(Crabs_Male_and_Orange$CW,Crabs_Female_and_Orange$CW,col = c("orange","orange"), main = "Espèce Orange - Comparaison de la variable CW selon le sexe")
+boxplot(Crabs_Male_and_Orange$CW, Crabs_Female_and_Orange$CW, col = c("grey","white"), names = c("Mâle","Femelle"), main = "Espèce Orange - Comparaison de la variable CW selon le sexe")
 #comparer le paramètre FL des crabes d'espèce orange en fonction du sexe (male/femelle)
-boxplot(Crabs_Male_and_Orange$BD,Crabs_Female_and_Orange$BD,col = c("orange","orange"), main = "Espèce Orange - Comparaison de la variable BD selon le sexe")
+boxplot(Crabs_Male_and_Orange$BD, Crabs_Female_and_Orange$BD, col = c("grey","white"), names = c("Mâle","Femelle"), main = "Espèce Orange - Comparaison de la variable BD selon le sexe")
 
 
 #1.2.2
