8fdd33447a805c6a67796a437dad912641ff5f0e
LU Han <hanlushdx@sina.com>
2017-03-24 16:05:54 +0100

    test chinois

--- TP1/src/1_1_Notes.R
+++ TP1/src/1_1_Notes.R
@@ -1,14 +1,14 @@
 # boxplot(x~y) diagramme en barre de x en fonction de y
 
 # factor = variable qualitative
-#   => qual ordinale (ordon√©e) ou nominale (simple nom)
+#   => qual ordinale (ordon®¶e) ou nominale (simple nom)
 # class(x) o√π X variable => donne la classe (entier, numerique, double, factor pour variable qualitative)
 #   => donne les "levels" de la variable, les valeurs qu'elle peut prendre
 
 
 # Tri
-# which(label == valeur) = selectionner donn√©es avec label = valeur
-# -which(label == valeur) = selectionner donn√©es avec label != valeur
+# which(label == valeur) = selectionner donn®¶es avec label = valeur
+# -which(label == valeur) = selectionner donn®¶es avec label != valeur
 
 
 # Remove Not Available NA
@@ -18,7 +18,7 @@
 
 
 # Tableau de contingence pour deux variables qualitatives
-tab_contingeance = table(notes$specialite, notes$resultat) # tableau de contingeance pour chaque sp√©cialit√©
+tab_contingeance = table(notes$specialite, notes$resultat) # tableau de contingeance pour chaque sp®¶cialit®¶
 barplot(tab_contingeance, beside = T) # diagramme en baton des resultats par branche
 
 # ------------------------------
@@ -28,68 +28,72 @@ notes <- read.csv("data/sy02-p2016.csv", na.strings="", header=T)
 # summary(notes)
 notes$nom <- factor(notes$nom, levels=notes$nom)
 notes$niveau <- factor(notes$niveau, ordered=T)
+notes$correcteur.median <- factor(notes$correcteur.median,
+                                  levels=c("Cor1","Cor2","Cor3","Cor4","Cor5","Cor6","Cor7","Cor8"))
+notes$correcteur.final <- factor(notes$correcteur.final,
+                                   levels=c("Cor1","Cor2","Cor3","Cor4","Cor5","Cor6","Cor7","Cor8"))
 notes$resultat <- factor(notes$resultat, levels=c("F","Fx","E","D","C","B","A"),ordered=T)
 
-# Question 1 : analyse g√©n√©rale 
+# Question 1 : analyse g®¶n®¶rale 
 dim(notes) # volume
 
-# outil pour la corr√©lation de deux variables quantitatives
-#   => test du Chi2 (X2) d'ind√©pendance (hypoth√®se = ind√©pendance)
-#    => v√©rifier condition n*prob th√©orique sous hyp h0 sup√©rieure ou √©gale √† 5
+# outil pour la corr®¶lation de deux variables quantitatives
+#   => test du Chi2 (X2) d'ind®¶pendance (hypoth®®se = ind®¶pendance)
+#    => v®¶rifier condition n*prob th®¶orique sous hyp h0 sup®¶rieure ou ®¶gale ®§ 5
 
-boxplot(notes$niveau~notes$resultat) # ne semble pas tr√®s li√©, et beaucoup de GX02 √† cause de la m√©diane sur la valeur 2
-boxplot(notes$note.totale~notes$specialite) # l√©g√®rement li√©, mais attention au nombre d'√©tudiants dans chaque cat√©gorie (ex : HuTech = un seul √©tudiant)
-boxplot(notes$note.totale~notes$resultat) # li√©, logique...
-plot(notes$note.median,notes$note.final) # peu li√©
-boxplot(notes$note.median~notes$correcteur.median) # un peu li√© mais √† √©tudier
-boxplot(notes$note.final~notes$correcteur.final) # un peu li√© mais √† √©tudier
-boxplot(notes$note.totale~notes$dernier.diplome.obtenu) # li√©
+boxplot(notes$niveau~notes$resultat) # ne semble pas tr®®s li®¶, et beaucoup de GX02 ®§ cause de la m®¶diane sur la valeur 2
+boxplot(notes$note.totale~notes$specialite) # l®¶g®®rement li®¶, mais attention au nombre d'®¶tudiants dans chaque cat®¶gorie (ex : HuTech = un seul ®¶tudiant)
+boxplot(notes$note.totale~notes$resultat) # li®¶, logique...
+plot(notes$note.median,notes$note.final) # peu li®¶
+boxplot(notes$note.median~notes$correcteur.median) # un peu li®¶ mais ®§ ®¶tudier
+boxplot(notes$note.final~notes$correcteur.final) # un peu li®¶ mais ®§ ®¶tudier
+boxplot(notes$note.totale~notes$dernier.diplome.obtenu) # li®¶
 
 
 
 # Question 2
 
-### Lien Resultat et formation d'origine des √©tudiants (dernier.diplome.obtenu)
+### Lien Resultat et formation d'origine des ®¶tudiants (dernier.diplome.obtenu)
 t(t(summary(notes$dernier.diplome.obtenu)))
 # On a des NA, quels sont-ils ?
-notes$statut[which(is.na(notes$dernier.diplome.obtenu))] # r√©sultat = [1] Echange Echange Echange Echange Echange Echange
-# => on se rend compte que les 6 donn√©es manquantes correspondent aux √©tudiants venant en √©change pour qui ce n'est pas renseign√©
-# => On va donc les enlever dans "dernier.diplome.obtenu" les lignes poss√©dant des valeurs manquantes (1)
-# Et on va aussi enlever dans "resultat" les lignes concernant les √©tudiants en Echange (2)
+notes$statut[which(is.na(notes$dernier.diplome.obtenu))] # r®¶sultat = [1] Echange Echange Echange Echange Echange Echange
+# => on se rend compte que les 6 donn®¶es manquantes correspondent aux ®¶tudiants venant en ®¶change pour qui ce n'est pas renseign®¶
+# => On va donc les enlever dans "dernier.diplome.obtenu" les lignes poss®¶dant des valeurs manquantes (1)
+# Et on va aussi enlever dans "resultat" les lignes concernant les ®¶tudiants en Echange (2)
 formation_origine = notes$dernier.diplome.obtenu[-which(is.na(notes$resultat))] # enlever les absents
 formation_origine
-formation_origine = na.omit(formation_origine) # (1) # enlever les diplomes non renseign√©s
+formation_origine = na.omit(formation_origine) # (1) # enlever les diplomes non renseign®¶s
 #resultats_sans_echange = notes$resultat[-which(notes$statut=="Echange")] # (2)
 resultats_sans_echange = notes$resultat[-which(is.na(notes$dernier.diplome.obtenu))] # (2)
 resultats_sans_echange
 resultats_sans_echange = na.omit(resultats_sans_echange)
-# v√©rifier que les deux nouvelles variables ont la m√™me longueur
-length(formation_origine) == length(resultats_sans_echange) # = TRUE => m√™me longueur/dimension
+# v®¶rifier que les deux nouvelles variables ont la m®∫me longueur
+length(formation_origine) == length(resultats_sans_echange) # = TRUE => m®∫me longueur/dimension
 # tableau de contingence
 contingence_result_formation = as.data.frame.matrix(table(formation_origine, resultats_sans_echange))
 contingence_result_formation
-# on remarque que beaucoup de cases du tableau n'ont pas un effectif sup√©rieur ou √©gal √† 5, les conditions pour le test du Chi 2 ne sont donc pas r√©unies.
+# on remarque que beaucoup de cases du tableau n'ont pas un effectif sup®¶rieur ou ®¶gal ®§ 5, les conditions pour le test du Chi 2 ne sont donc pas r®¶unies.
 # On va quand meme le faire pour voir ce que √ßa donne
 chisq.test(contingence_result_formation)
-# Test Chi2 : En g√©n√©ral on accepte l'hypoth√®se d'ind√©pendance lorsque p-value est sup√©rieure √† 5 % (0,05).
-# on remarque quand m√™me que certains dimplome d'origine semblent influer sur le r√©sultat. Par exemple, beaucoup plus de DUT √©chouent que les √©l√®ves ayant un BAC en dernier dil√¥me, cad les √©l√®ves venant de TC
-# donc les conditions du test n'√©tant pas respect√©e pr√©c√©dement, nous nous int√©ressement seulement aux population qui remplissent les conditions, √† savoir BAC, CPGE, DUT
+# Test Chi2 : En g®¶n®¶ral on accepte l'hypoth®®se d'ind®¶pendance lorsque p-value est sup®¶rieure ®§ 5 % (0,05).
+# on remarque quand m®∫me que certains dimplome d'origine semblent influer sur le r®¶sultat. Par exemple, beaucoup plus de DUT ®¶chouent que les ®¶l®®ves ayant un BAC en dernier dil√¥me, cad les ®¶l®®ves venant de TC
+# donc les conditions du test n'®¶tant pas respect®¶e pr®¶c®¶dement, nous nous int®¶ressement seulement aux population qui remplissent les conditions, ®§ savoir BAC, CPGE, DUT
 contingence_result_formation_BAC_DUT_CPGE = rbind(contingence_result_formation[4,], contingence_result_formation[6,], contingence_result_formation[8,])
 contingence_result_formation_BAC_DUT_CPGE
 chisq.test(contingence_result_formation_BAC_DUT_CPGE)
-# Bien que certains effectifs sont en dessous de 5, on obtient quand m√™me un test du Chi2 significatif avec p-value = 0.0006389
-# => on peut donc rejeter l'hypoth√®se d'ind√©pendance => la formation d'origine des √©tudiants a un impact sur leur r√©sultat final
+# Bien que certains effectifs sont en dessous de 5, on obtient quand m®∫me un test du Chi2 significatif avec p-value = 0.0006389
+# => on peut donc rejeter l'hypoth®®se d'ind®¶pendance => la formation d'origine des ®¶tudiants a un impact sur leur r®¶sultat final
 
 
 
 
 
-### Lien Resultat et Sp√©cialit√© des √©tudiants (GB, GI, ...)
+### Lien Resultat et Sp®¶cialit®¶ des ®¶tudiants (GB, GI, ...)
 t(t(summary(notes$specialite))) # pas de NA mais on remarque des classes avec peu d'effectif, il va donc falloir les supprimer pour faire un test de Chi2
 
 contingence_resut_speciality = as.data.frame.matrix(table(notes$specialite, notes$resultat))
 contingence_resut_speciality
-chisq.test(contingence_resut_speciality) #p-value = 0.02088 < 0,05 donc on refute l'hypoth√®se d'ind√©pendance => les variables sont d√©pendantes
+chisq.test(contingence_resut_speciality) #p-value = 0.02088 < 0,05 donc on refute l'hypoth®®se d'ind®¶pendance => les variables sont d®¶pendantes
 filtered_contingence_resut_speciality = rbind(
   contingence_resut_speciality[1,], 
   contingence_resut_speciality[2,], 
@@ -98,22 +102,22 @@ filtered_contingence_resut_speciality = rbind(
   contingence_resut_speciality[6,]
 )
 filtered_contingence_resut_speciality
-chisq.test(filtered_contingence_resut_speciality) # p-value = 0.4853 largement sup√©rieur √† 0,05 donc on accepte avec grande confiance l'yphoth√®se d'un
-plot(filtered_contingence_resut_speciality) # ?????????? comment mod√©liser graphiquement ?
+chisq.test(filtered_contingence_resut_speciality) # p-value = 0.4853 largement sup®¶rieur ®§ 0,05 donc on accepte avec grande confiance l'yphoth®®se d'un
+plot(filtered_contingence_resut_speciality) # ?????????? comment mod®¶liser graphiquement ?
 
 
-### Lien Resultat et leur niveau (GX 1 √† 6)
+### Lien Resultat et leur niveau (GX 1 ®§ 6)
 t(t(summary(notes$niveau))) 
 contingence_resut_niveau = as.data.frame.matrix(table(notes$niveau, notes$resultat))
 contingence_resut_niveau
-chisq.test(contingence_resut_niveau) # condition non remplie, p-value = 0.0003518 => rejet hypoth√®se ind√©pendance
+chisq.test(contingence_resut_niveau) # condition non remplie, p-value = 0.0003518 => rejet hypoth®®se ind®¶pendance
 filtered_contingence_resut_niveau = rbind(
   contingence_resut_niveau[1,], 
   contingence_resut_niveau[2,], 
   contingence_resut_niveau[4,]
 )
 filtered_contingence_resut_niveau
-chisq.test(filtered_contingence_resut_niveau) # p-value = 0.002587 => rejet hypoth√®se ind√©pendance, plus le niveau monte (plus le temps passe pour l'√©tudiant), plus il semble difficile de r√©ussir l'UV
+chisq.test(filtered_contingence_resut_niveau) # p-value = 0.002587 => rejet hypoth®®se ind®¶pendance, plus le niveau monte (plus le temps passe pour l'®¶tudiant), plus il semble difficile de r®¶ussir l'UV
 
 
 
@@ -130,7 +134,7 @@ classes_notes_median_clean = cut(notes_median_clean,
 contingence_result_correcteur_median = as.data.frame.matrix(table(cor_median_clean, classes_notes_median_clean))
 #contingence_result_correcteur_median = as.data.frame.matrix(table(classes_notes_median_clean, cor_median_clean))
 contingence_result_correcteur_median
-chisq.test(contingence_result_correcteur_median) # p-value = 0.08142 => on accepte l'hypoth√®se d'ind√©pendance mais de peu
+chisq.test(contingence_result_correcteur_median) # p-value = 0.08142 => on accepte l'hypoth®®se d'ind®¶pendance mais de peu
 
 
 
@@ -145,4 +149,4 @@ classes_notes_final_clean = cut(notes_final_clean,
                                  right = FALSE) # regrouper les notes en classes
 contingence_result_correcteur_final = as.data.frame.matrix(table(cor_final_clean, classes_notes_final_clean))
 contingence_result_correcteur_final
-chisq.test(contingence_result_correcteur_final) # p-value = 0.05737 => on accepte l'hypoth√®se d'ind√©pendance mais de tr√®s peu
+chisq.test(contingence_result_correcteur_final) # p-value = 0.05737 => on accepte l'hypoth®®se d'ind®¶pendance mais de tr®®s peu
--- TP1/src/PCA.R
+++ TP1/src/PCA.R
@@ -11,27 +11,27 @@ summary(notes)
 
 
 ###########################################
-# 2.1 Exercice théorique
+# 2.1 Exercice th茅orique
 
-#calculer les moyennes des notes médian corrigées par chaque correcteur 
+#calculer les moyennes des notes m茅dian corrig茅es par chaque correcteur 
 #aggregate: Splits the data into subsets, computes summary statistics for each, and returns the result in a convenient form.
 moy.median <- aggregate(note.median~correcteur.median, data=notes, FUN=mean)
-#renommer la dataframe “moy.median”
+#renommer la dataframe 鈥渕oy.median鈥?
 moy.median
 names(moy.median) <- c("correcteur","moy.median")
 moy.median
-#calculer les écart-types des notes médian corrigées par chaque correcteur 
+#calculer les 茅cart-types des notes m茅dian corrig茅es par chaque correcteur 
 std.median <- aggregate(note.median~correcteur.median, data=notes, FUN=sd)
 names(std.median) <- c("correcteur","std.median")
 std.median
 #mettre deux datadframe ensemble 
 median <- merge(moy.median, std.median)
 median
-#calculer les moyennes des notes finales corrigées par chaque correcteur 
+#calculer les moyennes des notes finales corrig茅es par chaque correcteur 
 moy.final <- aggregate(note.final~correcteur.final, data=notes, FUN=mean)
 names(moy.final) <- c("correcteur","moy.final")
 moy.final
-#calculer les écarts types des notes finales corrigées par chaque correcteur 
+#calculer les 茅carts types des notes finales corrig茅es par chaque correcteur 
 std.final <- aggregate(note.final~correcteur.final, data=notes, FUN=sd)
 names(std.final) <- c("correcteur","std.final")
 std.final
@@ -45,33 +45,33 @@ corr.acp <- correcteurs[-c(2,3),] # on peut aussi faire merge(median, final) san
 corr.acp
 
 
-# 1. Calculer les axes factoriels de l’ACP du nuage de points défini
+# 1. Calculer les axes factoriels de l鈥橝CP du nuage de points d茅fini
 # par les quatre variables quantitatives.
-# Quels sont les pourcentages d’inertie expliquée par chacun de ces axes ?
+# Quels sont les pourcentages d鈥檌nertie expliqu茅e par chacun de ces axes ?
 corr.acp <- corr.acp[,-c(1)] # ne prendre que les variables quantitatives
 corr.acp
-corr.acp.centered = scale(corr.acp, center = TRUE, scale = FALSE) # centrer les données en colonne
+corr.acp.centered = scale(corr.acp, center = TRUE, scale = FALSE) # centrer les donn茅es en colonne
 corr.acp.centered
-corr.acp.covar = cov.wt(corr.acp.centered, method = "ML") # calcul de matrice de covariance avec centrage et réduction automatique
+corr.acp.covar = cov.wt(corr.acp.centered, method = "ML") # calcul de matrice de covariance avec centrage et r茅duction automatique
 corr.acp.covar
 corr.acp.covar.diago = eigen(corr.acp.covar$cov) # diagonaliser la covariance (pas les moyennes)
-val_propres = corr.acp.covar.diago$values # valeurs propres = quantités d'inertie expliquée
+val_propres = corr.acp.covar.diago$values # valeurs propres = quantit茅s d'inertie expliqu茅e
 vec_propres = corr.acp.covar.diago$vectors # vecteurs propres = vecteurs qui portent/expliquent les axes factoriels
 # normalement matrice de covariance = corr.acp.covar.diago$vectors * corr.acp.covar.diago$values * t(corr.acp.covar.diago$vectors)
 vec_propres %*% diag(val_propres) %*% t(vec_propres)
 corr.acp.covar$cov
-# on remarque que les deux sont egales, même si le comparateur "==" ne marche pas car pas problème de précision
+# on remarque que les deux sont egales, m锚me si le comparateur "==" ne marche pas car pas probl猫me de pr茅cision
 
-# Calculer les axes factoriels de l’ACP
-# les axes factoriels sont donnés par les vecteurs propres situés dans vec_propres : chaque colonne est un des vecteur propre
+# Calculer les axes factoriels de l鈥橝CP
+# les axes factoriels sont donn茅s par les vecteurs propres situ茅s dans vec_propres : chaque colonne est un des vecteur propre
 vec_propres 
-# les quantités d'inertie expliquées sont dans la variable val_propres
+# les quantit茅s d'inertie expliqu茅es sont dans la variable val_propres
 val_propres
-# les pourcentages d'inerties sont simplement la quantité / la somme de la quantité totale d'inertie
+# les pourcentages d'inerties sont simplement la quantit茅 / la somme de la quantit茅 totale d'inertie
 pourcentage_inertie = val_propres / sum(val_propres) * 100
 pourcentage_inertie
 
-# 2. Calculer les composantes principales ; en déduire la représentation des quatre individus dans le premier plan factoriel.
+# 2. Calculer les composantes principales ; en d茅duire la repr茅sentation des quatre individus dans le premier plan factoriel.
 ACP = corr.acp.centered %*% vec_propres
-ACP # coordonnées des points sur le nouveau repère des composantes principales = combinaisons linéaires des anciens axes
+ACP # coordonn茅es des points sur le nouveau rep猫re des composantes principales = combinaisons lin茅aires des anciens axes
 
