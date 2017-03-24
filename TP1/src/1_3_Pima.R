6e3dcaed2c3931a6f3848e6f0154db824d83ea4f
raphaelhamonnais <raphael.hamonnais@gmail.com>
2017-03-24 16:21:05 +0100

    utf 8 suite

--- TP1/src/1_3_Pima.R
+++ TP1/src/1_3_Pima.R
@@ -1,19 +1,19 @@
-#1.3 Donnees Pima
-Pima <- read.csv("E:/study/UTC/GI05/SY09/TD/TD1/donnees/Pima.csv",header = T)
+#1.3 Données Pima
+Pima <- read.csv("data/Pima.csv",header = T)
 summary(Pima)
 plot(Pima)
 Pima$z <- factor(Pima$z)
 dim(Pima)
-#supprimer la derni��re colonne 
+#supprimer la dernière colonne 
 Pima <- Pima[,-ncol(Pima)]
 Pima
-#prendre les premiers six lignes 
+#prendre les six premirèes lignes
 head(Pima)
 summary(Pima)
 
 hist(Pima$npreg)
 plot(density(Pima$glu))
-#sort(): pour faire l 'ordre des donnees
+#sort(): pour faire l 'ordre des données
 plot(sort(Pima$bp))
 plot(Pima$ped~Pima$bp)
 boxplot(Pima$ped~Pima$glu)
