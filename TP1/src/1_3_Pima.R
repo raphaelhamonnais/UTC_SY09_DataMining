fad1c07fb610effaf29913876671dc57d60d2ec8
raphaelhamonnais <raphael.hamonnais@gmail.com>
2017-03-26 16:13:46 +0200

    Travail sur Pima, pas grand chose

--- TP1/src/1_3_Pima.R
+++ TP1/src/1_3_Pima.R
@@ -7,7 +7,7 @@ dim(Pima)
 #supprimer la dernière colonne 
 Pima <- Pima[,-ncol(Pima)]
 Pima
-#prendre les six premirèes lignes
+#prendre les six premières lignes
 head(Pima)
 summary(Pima)
 
@@ -16,6 +16,38 @@ plot(density(Pima$glu))
 #sort(): pour faire l 'ordre des données
 plot(sort(Pima$bp))
 plot(Pima$ped~Pima$bp)
-boxplot(Pima$ped~Pima$glu)
-
+boxplot(Pima$npreg~Pima$z)
+boxplot(Pima$glu~Pima$z)
+boxplot(Pima$bp~Pima$z)
+boxplot(Pima$skin~Pima$z)
+boxplot(Pima$bmi~Pima$z)
+boxplot(Pima$ped~Pima$z)
+boxplot(Pima$age~Pima$z)
 cor(Pima)
+
+
+a = table(Pima$z[which(Pima$z == 1)], Pima$npreg[which(Pima$z == 1)])
+a = as.data.frame(table(Pima$z, Pima$npreg))
+a
+sum(a[1,])
+sum(a[2,])
+table(Pima$z, Pima$glu)
+plot(Pima$z~Pima$glu)
+table(Pima$z, Pima$bp)
+table(Pima$z, Pima$skin)
+table(Pima$z, Pima$bmi)
+table(Pima$z, Pima$ped)
+table(Pima$z, Pima$age)
+
+
+
+acp = princomp(Pima)
+acp
+biplot(acp)
+
+acp$scores
+plot(acp$scores[,2]~acp$scores[,1], col = c("blue","orange")[Pima$z])
+plot(acp$scores[,1]~acp$scores[,7], col = c("blue","orange")[Pima$z])
+
+
+
