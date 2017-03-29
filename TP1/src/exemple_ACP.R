6deb94dfb5ca9fa74df1df257018e3fd8701a435
LU Han <hanlushdx@sina.com>
2017-03-29 16:39:16 +0200

    graphies de l'exemple ACP dans le poly

--- TP1/src/exemple_ACP.R
+++ TP1/src/exemple_ACP.R
@@ -1,3 +1,6 @@
+install.packages("plotrix")
+library("plotrix")
+
 #un exemple dans le poly de page 43
 math <- t(t(c(6,8,6,14.5,14,11,5.5,13,9)))
 math
@@ -31,11 +34,11 @@ notes_covariance_diag
 valeurs_propres <- notes_covariance_diag$values
 valeurs_propres
 #vecteurs propres
-vecteurs_propres <- (notes_covariance_diag$vectors)
+vecteurs_propres <- notes_covariance_diag$vectors
 vecteurs_propres
 
 #la matrice des composantes principales
-ACP <- notes_centrage%*%vecteurs_propres
+ACP <- notes_centrage %*% vecteurs_propres
 ACP
 
 #Vue que les pourcentages d'inertie expliquée par chaque axes sont 57.69, 24.65, 17.59, 0.04, 0.02
@@ -45,19 +48,62 @@ ACP
 plot(-9:10,-9:10,type = "n", xlab = "Axe1", ylab = "Axe2")
 abline(h=0,v=0)
 #Ajouter des points dans le premier plan factoriel 
-text(ACP[,1], ACP[,2])
+text(ACP[,1], ACP[,2], labels = c("jean", "aline", "annie", "monique", "didier", "andre", "pierre", "brigitte", "evelyne"))
 
 #Dessiner le premier plan factoriel entre composant1 et composant3
 plot(-9:10,-9:10,type = "n", xlab = "Axe1", ylab = "Axe3")
 abline(h=0,v=0)
 #Ajouter des points dans le premier plan factoriel 
-text(ACP[,1], ACP[,3])
+text(ACP[,1], ACP[,3], labels = c("jean", "aline", "annie", "monique", "didier", "andre", "pierre", "brigitte", "evelyne"))
 
 #Dessiner le premier plan factoriel entre composant1 et composant4
 plot(-9:10,-9:10,type = "n", xlab = "Axe1", ylab = "Axe4")
 abline(h=0,v=0)
 #Ajouter des points dans le premier plan factoriel 
-text(ACP[,1], ACP[,4])
+text(ACP[,1], ACP[,4], labels = c("jean", "aline", "annie", "monique", "didier", "andre", "pierre", "brigitte", "evelyne"))
 
+#Calculer des contributions 
+#contributions relatives des axes aux individus
+# apply(notes_centrage^2, 1, sum)
+# 1/apply(notes_centrage^2, 1, sum)
+# diag(1/apply(notes_centrage^2, 1, sum))
+# ACP^2
+COR <- diag(1/apply(notes_centrage^2, 1, sum)) %*% ACP^2
+COR
 
+#contributions relatives des individus aux axes
+n <- dim(notes)[1]
+# (1/n)*ACP^2
+# 1/diag(valeurs_propres)
+# diag(1/diag(valeurs_propres))
+# diag(diag(1/diag(valeurs_propres)))
+CTR <- (1/n)*ACP^2 %*% diag(diag(1/diag(valeurs_propres)))
+CTR
 
+#representation des variables
+# sqrt((n - 1)/n)
+# apply(notes_centrage,2,sd)
+# (sqrt((n - 1)/n)*apply(notes_centrage,2,sd))
+# 1/(sqrt((n - 1)/n)*apply(notes_centrage,2,sd))
+# diag(1/(sqrt((n - 1)/n)*apply(notes_centrage,2,sd)))
+# diag(1/(sqrt((n - 1)/n)*apply(notes_centrage,2,sd))) %*% vecteurs_propres 
+# diag(sqrt(valeurs_propres))
+cor <- diag(1/(sqrt((n - 1)/n)*apply(notes_centrage,2,sd))) %*% vecteurs_propres %*% diag(sqrt(valeurs_propres))
+rownames(cor) <- c("math", "scie", "fran", "lati", "d-m")
+colnames(cor) <- c("F1", "F2", "F3", "F4", "F5")
+cor
+
+plot(-1:1, -1:1, type = "n", xlab = "Axe1", ylab = "Axe2")
+abline(h=0,v=0)
+draw.circle(0,0,1)
+text(cor[,1], cor[,2], labels = c("math", "scie", "fran", "lati", "d-m"))
+
+plot(-1:1, -1:1, type = "n", xlab = "Axe1", ylab = "Axe3")
+abline(h=0,v=0)
+draw.circle(0,0,1)
+text(cor[,1], cor[,3], labels = c("math", "scie", "fran", "lati", "d-m"))
+
+plot(-1:1, -1:1, type = "n", xlab = "Axe1", ylab = "Axe4")
+abline(h=0,v=0)
+draw.circle(0,0,1)
+text(cor[,1], cor[,4], labels = c("math", "scie", "fran", "lati", "d-m"))
